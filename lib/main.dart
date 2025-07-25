2d904f54f1135f76cd8790b83f4d92f60db8fe72
Jan Neumann <jan.alexander.neumann@gmail.com>
2025-07-25 15:10:36 +0200

    add google map api functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:favorite_places/screens/places_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:google_fonts/google_fonts.dart';
@@ -26,7 +27,8 @@ final theme = ThemeData().copyWith(
   ),
 );
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(
       const MyApp(),
   );
--- lib/model/place.dart
+++ lib/model/place.dart
@@ -2,10 +2,26 @@ import 'dart:io';
 
 import 'package:uuid/uuid.dart';
 
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Place {
-  Place({required this.image, required this.name}) : id = Uuid().v4();
- 
+  Place(
+    // this.location, 
+  {required this.image, required this.name})
+    : id = Uuid().v4();
+
   final String id;
   final String name;
   final File image;
+ // final PlaceLocation location;
 }
